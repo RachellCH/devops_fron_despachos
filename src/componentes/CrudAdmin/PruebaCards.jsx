307f65f2f8d6013fc3cad8b0ee7a527fed3fa274
Rachell <ra.chavarria@duocuc.cl>
2026-07-08 21:17:25 -0400

    b

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra 💰b2"
+          title="Consultar Ordenes de compra 💰b"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
